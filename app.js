1ac81492df6e6a118f43ba9eb2335dcba36fa0b2
Лиза <lolliza04@gmail.com>
2026-09-03 08:52:36 +0300

    test: add screen client integration

--- app.js
+++ app.js
@@ -1,43 +1,33 @@
-const express = require("express");
+const express = require('express');
 const app = express();
+const DivoomClient = require('./src/client');
+const Screen = require('./src/commands/screen');
+const client = new DivoomClient('192.168.1.41');
+const screen = new Screen(client);
 
+screen.turnOn();
 
-app.use('/static', express.static('C:/Users/itach/OneDrive/Изображения/Тест картинки'))
+async function main() {
+  const data = await client.sendCommand({
+    Command: 'Channel/OnOffScreen',
+    OnOff: 1,
+  });
 
-async function testDivoom() {
-    try {
-        const response = await fetch(
-            'http://192.168.1.41:9000/divoom_api',
-            {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json'
-                },
-                body: JSON.stringify({
-                    Command: 'Channel/OnOffScreen',
-                    OnOff: 1
-                })
-            }
-        );
-
-        const data = await response.text();
-
-        console.log('Статус:', response.status);
-        console.log('Ответ от Divoom:', data);
-    } catch (error) {
-        console.error('Ошибка подключения к Divoom:', error.message);
-    }
+  console.log(data.ReturnMessage);
 }
+main();
 
-testDivoom();
+app.use(
+  '/static',
+  express.static('C:/Users/itach/OneDrive/Изображения/Тест картинки'),
+);
 
 app.use((req, res) => {
-    return res.status(404).send({
-        error: 'Прости брат тут ничего нет'
-    })
+  return res.status(404).send({
+    error: 'Прости брат тут ничего нет',
+  });
 });
 
-
-app.listen(3000, "0.0.0.0", () => {
-    console.log("Server running on port 3000");
+app.listen(3000, '0.0.0.0', () => {
+  console.log('Server running on port 3000');
 });
