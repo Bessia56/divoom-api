36a8b5b5a39810e9ab24fe429445ad27e698d869
Лиза <lolliza04@gmail.com>
2026-09-04 09:43:53 +0300

    Add weather location testAdd weather location test

--- app.js
+++ app.js
@@ -1,20 +1,26 @@
 const express = require('express');
 const app = express();
+
 const DivoomClient = require('./src/client');
 const Screen = require('./src/commands/screen');
+
 const client = new DivoomClient('192.168.1.41');
 const screen = new Screen(client);
 
-screen.turnOn();
-
 async function main() {
-  const data = await client.sendCommand({
-    Command: 'Channel/OnOffScreen',
-    OnOff: 1,
-  });
+  try {
+    // await screen.turnOn();
+    // await screen.setBrightness(100);
+    // await screen.setMirrorMode(45);
+    // await screen.setHourMode(0);
+    const result = await screen.setWeatherLocation(39.7248, 43.5970);
 
-  console.log(data.ReturnMessage);
+    console.log(result);
+  } catch (error) {
+    console.log(error.message);
+  }
 }
+
 main();
 
 app.use(
