b76f0d36fec4aeb36f0a16a632583feb89458114
Лиза <lolliza04@gmail.com>
2026-09-24 13:58:25 +0300

    Add DisplayElement class

---
+++ src/elements/displayElement.js
@@ -0,0 +1,12 @@
+class DisplayElement {
+  constructor(key, type, options) {
+    this.key = key;
+    this.type = type;
+
+    this._options = options;
+  }
+}
+
+module.exports = {
+  DisplayElement,
+};
