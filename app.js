c32edc1562ccc901d1554a324777152add1b89ed
Лиза <lolliza04@gmail.com>
2026-09-24 13:59:52 +0300

    "Update display element tests

--- app.js
+++ app.js
@@ -4,108 +4,48 @@ const app = express();
 const DivoomClient = require('./src/client');
 const Screen = require('./src/commands/screen');
 const CustomDisplay = require('./src/commands/customDisplay');
+const DisplayLayout = require('./src/commands/displayLayout');
+const { DisplayElement } = require('./src/elements/displayElement');
 
 const client = new DivoomClient('192.168.1.41');
 const screen = new Screen(client);
-const customDisplay = new CustomDisplay(
+const customDisplay = new CustomDisplay();
+const displayLayout = new DisplayLayout(
   client,
   'http://192.168.1.40:3000/static/the-boxhead-wanderer-pr-800x1280.jpg',
 );
-
-// async function main() {
-//   try {
-//     // await screen.turnOn();
-//     // await screen.setBrightness(100);
-//     // await screen.setMirrorMode(45);
-//     // await screen.setHourMode(0);
-//     const result = await screen.setWeatherLocation(39.7248, 43.597);
-
-//     console.log(result);
-//   } catch (error) {
-//     console.log(error.message);
-//   }
-// }
+const convertDisplayElement = require('./src/converters/displayElementConverter');
 
 async function main() {
-  try {
-    // const result = await customDisplay.showText('Привет, Divoom!', {
-    //   x: 0,
-    //   y: 10,
-    //   width: 720,
-    //   height: 90,
-    //   align: 2,
-    //   fontSize: 64,
-    //   fontId: 126,
-    //   fontColor: '#FFFFFF',
-    //   bgColor: '#000000',
-    // });
-    // const result = await customDisplay.showImage(
-    //   'https://f.divoom-gz.com/320320.gif',
-    //   {
-    //     x: 13,
-    //     y: 227,
-    //     width: 320,
-    //     height: 320,
-    //   },
-    // );
-    const result = await customDisplay.showTime({
-      // x: 474,
-      // y: 1123,
-      // width: 308,
-      // height: 125,
-      // align: 1,
-      // fontSize: 95,
-      // fontId: 52,
-      // fontColor: '#A8B35E',
-      // bgColor: '#FF0000',
-    });
-    // const result = await customDisplay.showMday({
-    //   x: 200,
-    //   y: 550,
-    //   width: 400,
-    //   height: 150,
-    //   align: 2,
-    //   fontSize: 100,
-    //   fontId: 52,
-    //   fontColor: '#FFFFFF',
-    //   bgColor: '#000000',
-    // });
-    // const result = await customDisplay.showMonYear({
-    //   x: 100,
-    //   y: 500,
-    //   width: 600,
-    //   height: 200,
-    //   align: 2,
-    //   fontSize: 70,
-    //   fontId: 52,
-    //   fontColor: '#FFFFFF',
-    //   bgColor: '#000000',
-    // });
-    // const result = await customDisplay.showWeek({
-    //   x: 100,
-    //   y: 500,
-    //   width: 600,
-    //   height: 200,
-    //   align: 2,
-    //   fontSize: 70,
-    //   fontId: 52,
-    //   fontColor: '#FFFFFF',
-    //   bgColor: '#000000',
-    // });
-    // const result = await customDisplay.showTemperature({
-    //   x: 100,
-    //   y: 500,
-    //   width: 300,
-    //   height: 100,
-    //   align: 2,
-    //   fontSize: 70,
-    //   fontId: 52,
-    //   fontColor: '#FFFFFF',
-    //   bgColor: '#000000',
-    // });
-  } catch (error) {
-    console.log(error.message);
-  }
+  const temperature = await customDisplay.showTemperature('temperature', {
+    x: 50,
+    y: 100,
+    width: 150,
+    height: 70,
+    align: 0,
+    fontSize: 20,
+    fontId: 0,
+    fontColor: '#FFFFFF',
+    bgColor: '#000000',
+  });
+
+  const text = await customDisplay.showText('greeting', 'Hello world', {
+    x: 20,
+    y: 200,
+    width: 200,
+    height: 50,
+    align: 0,
+    fontSize: 20,
+    fontId: 0,
+    fontColor: '#FFFFFF',
+    bgColor: '#000000',
+  });
+
+  console.log('Temperature:');
+  console.log(convertDisplayElement(temperature));
+
+  console.log('Text:');
+  console.log(convertDisplayElement(text));
 }
 
 main();
