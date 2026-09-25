44f6d4769bd2db4e11b35a41e5210d3d537a2c71
Лиза <lolliza04@gmail.com>
2026-09-25 08:53:48 +0300

    Add display element conversion

--- src/converters/displayElementConverter.js
+++ src/converters/displayElementConverter.js
@@ -20,16 +20,28 @@ function convertDisplayElement(element) {
   }
   if (element.type === 'Text') {
     return {
-      ...createBaseElement(element, 5),
+      ...createBaseElement(element, 13),
       TextMessage: element._options.text,
     };
   }
   if (element.type === 'Image') {
     return {
-      ...createBaseElement(element, 13),
+      ...createBaseElement(element, 12),
       Url: element._options.url,
       ImgLocalFlag: 0,
     };
+  }
+  if (element.type === 'Time') {
+    return createBaseElement(element, 4);
+  }
+  if (element.type === 'Mday') {
+    return createBaseElement(element, 5);
+  }
+  if (element.type === 'MonYear') {
+    return createBaseElement(element, 6);
+  }
+  if (element.type === 'Week') {
+    return createBaseElement(element, 7);
   }
 
   throw new Error(`Unsupported display element type: ${element.type}`);
